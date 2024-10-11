20bfd2166c76d48a641265a1779e22c260a4c96e
celine <imanica@g.cofc.edu>
2024-10-11 13:11:21 -0400

    Update Sign Up
    Should now register users into the Firebase Authentication database.

--- src/Pages/Sign Up/Sign Up/Signup.jsx
+++ src/Pages/Sign Up/Sign Up/Signup.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 import './Signup.css';
 import { FaUser, FaLock } from "react-icons/fa";
 import logo from './BlackLogo.png';
 import { Link } from 'react-router-dom';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../../Config/firebase';
 
 
+const Signup =  () => {
+    const [registerEmail, setRegisterEmail] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
 
-const Signup = () => {
+    const handleRegisterClick = async (event) => { 
+        event.preventDefault();
+     try {
+        const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+        console.log("User created:", user)
+         } catch (error) {
+            console.log("Error creating user:", error.message);
+    }
+};
+
+
     return(
         <div className='wrapper'>
             <form action="">
@@ -29,17 +45,27 @@ const Signup = () => {
                     <FaUser className='icon' />
                 </div>
                 <div className="input-box">
-                    <input type="text" placeholder='Email' required />
+                    <input 
+                        type="text" 
+                        placeholder='Email' 
+                        onChange={(event) => setRegisterEmail(event.target.value) } 
+                        required
+                    />
                     <FaUser className='icon' />
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder='Password' required />
+                    <input 
+                        type="password" 
+                        placeholder='Password' 
+                        onChange={(event) => setRegisterPassword(event.target.value) } 
+                        required 
+                    />
                     <FaLock className='icon' />
                 </div>
                 <div className="register-link">
                     <p> Already have an account? <Link to="/Login">Login</Link></p>
                 </div>
-                <button type="submit">Create</button>
+                <button type="submit" onClick={handleRegisterClick}>Create</button>
             </form>
             </div>
     );
