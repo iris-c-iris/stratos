2abcaa7eac5cd943abac9fc3bbc7824ec4528eb7
Iris <119714707+iris-c-iris@users.noreply.github.com>
2024-10-12 17:09:37 -0400

    more updates

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     return (
         <header className="header">
             <div className="header-content">
-                <img src={logoOnlyW} alt="Logo" className="header-logo"/>
+                <img src={logoOnlyW} alt="Logo" className="header-logo" />
                 <h1>STRATOS WMS</h1>
             </div>
             <div className="search-bar-container">
