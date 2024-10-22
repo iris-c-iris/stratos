c8a00ce1007120bbf7306b5e8cd15af8214c511a
SMBuck <97459340+SMBuck@users.noreply.github.com>
2024-10-22 13:51:04 -0400

    Login buttons

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<TestPage/>} />
-                    <Route path="/TestPage" element={<TestPage/>}/>
-                    {/*<Route index element={<Login/>} />*/}
-                    {/*<Route path="/Login" element={<Login/>}/>*/}
-                    {/*<Route path="/Signup" element={<Signup/>}/>*/}
+                    {/*<Route index element={<TestPage/>} />
+                    <Route path="/TestPage" element={<TestPage/>*/}
+                    {<Route index element={<Login/>} />}
+                    {<Route path="/Login" element={<Login/>}/>}
+                    {<Route path="/Signup" element={<Signup/>}/>}
                 </Routes>
             </BrowserRouter>
         </div>
