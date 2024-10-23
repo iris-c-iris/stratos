a7ad9ee8fc175d2b20896a8c0eb4adeda3e62d5b
SMBuck <97459340+SMBuck@users.noreply.github.com>
2024-10-23 14:47:31 -0400

    Add Employee page

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login/Login/Login';
 import Signup from './Pages/Sign Up/Sign Up/Signup';
-import TestPage from './Pages/TestPage/TestPage/TestPage';
+import TestPage from './Pages/TestPage/TestPage/TestPage'; 
+import AddEmployee from './Pages/Add Employee/AddEmployee';
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<TestPage/>} />
-                    <Route path="/TestPage" element={<TestPage/>}/>
-                    {/*<Route index element={<Login/>} />*/}
-                    {/*<Route path="/Login" element={<Login/>}/>*/}
-                    {/*<Route path="/Signup" element={<Signup/>}/>*/}
+                    {/* <Route index element={<TestPage/>} />
+                    <Route path="/TestPage" element={<TestPage/>}/> */}
+                     {/* { <Route index element={<Login/>} /> }
+                    <Route path="/Login" element={<Login/>}/>
+                    <Route path="/Signup" element={<Signup/>}/>  */}
+                    <Route index element={<AddEmployee/>}/> 
                 </Routes>
             </BrowserRouter>
         </div>
