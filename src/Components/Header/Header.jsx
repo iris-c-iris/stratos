829151e41e34af144eb1b2e8fd16e29e694b368f
Iris <119714707+iris-c-iris@users.noreply.github.com>
2024-10-24 16:02:18 -0400

    small changes to header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -19,15 +19,7 @@ const Header = () => {
                         <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/28e05789d380bcc10d6936d90c515e6f3a15b2aaf384905df7377d98d95400ea?placeholderIfAbsent=true&apiKey=d7229136ad094bbf8efa03382e544f03" alt="Search" />
                     </button>
                 </form>
-                <button className="icon-button" aria-label="Notifications">
-                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/e6634ded01afef5bb8d11059ab5d92d789eae5c88ff6f30d880ae6fc4a6b2b7f?placeholderIfAbsent=true&apiKey=d7229136ad094bbf8efa03382e544f03" alt="" />
-                </button>
-                <button className="icon-button" aria-label="Settings">
-                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/2d762d081bde4fd7d793d352b09f7cd0b7530428e44fb5cdf8e7763db25eb0bb?placeholderIfAbsent=true&apiKey=d7229136ad094bbf8efa03382e544f03" alt="" />
-                </button>
-                <button className="icon-button" aria-label="Messages">
-                    <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b5bef16fc1431bcbf834ea57580ff05fb8708149bb8ca39715f73c29693d3ec8?placeholderIfAbsent=true&apiKey=d7229136ad094bbf8efa03382e544f03" alt="" />
-                </button>
+
                 <button className="icon-button profile-button" aria-label="Profile">
                     <img src={profile} alt="Profile" className="profile-button" />
                 </button>
